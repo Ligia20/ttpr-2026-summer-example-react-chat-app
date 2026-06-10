b8ea54ecfabb7c582599d9936ed3f2bb4e9478fe
Ligia <ligiaarevalo251@gmail.com>
2026-06-09 22:17:50 -0400

    commented out new features that still have bugs

--- src/App.tsx
+++ src/App.tsx
@@ -26,25 +26,41 @@ const App = () => {
         // map messages to <p> elements
         
             messages.map(
-                // @ts-expect-error
+                //
                 (text, index) => <Message key={index} text={text} index={index} />
                 )
         }
         <form onSubmit={(event) => {
             event.preventDefault();
             const new_message = event.target.incoming_text.value;
-            // @ts-expect-error
+            
             setMessages([...messages, new_message]);
             //note: this is not best practice in react but it works for now
-            //@ts-expect-error
+           
             document.getElementById('incoming_text').value = '';
     }}>
     <input name='incoming_text' id='incoming_text' />
     <button type='submit' className='send_button'>
         Send
-        
     </button>
     </form>
+    {/*Working on this to create a send button for each user, but it is not working yet.
+         <form onSubmit={(event) => {
+            event.preventDefault();
+            const new_message = event.target.incoming_text2.value;
+            
+            setMessages([...messages, new_message]);
+            //note: this is not best practice in react but it works for now
+           
+            document.getElementById('incoming_text2').value = '';
+    }}>
+    <input name='incoming_text2' id='incoming_text2' />
+    <button type='submit' className='send_button2'>
+        Send
+    </button>
+    </form>
+    */}
+    
 </>;
 }
 
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,14 +3,28 @@ import {faker} from "@faker-js/faker";
     const name1 = faker.person.fullName();
     const name2 = faker.person.fullName();
     const chatters = [name1, name2];
+   /* const currentSender = chatters[senderIndex];
+    const alignment = senderIndex === 0 ? 'left' : 'right'; */
 
-const Message = ({text}: {text:string} , {index}: {index:number}) => {
+    const Message = ({text, index}: {text: string, index: number}) => {
     const currentSender = chatters[index % 2];
     return <>
         <p className='message'>
-            <p className='message-text'>{text}</p> - <strong className='sender'> {currentSender} </strong>
+            <strong className='message_text'>{text} </strong> 
+            <p className='sender'> {currentSender} </p>
         </p>
         </>;
 }
-
+    
+/*  Working on this to create a send button for each user, but it is not working yet. 
+    const Message = ({text, senderIndex}: {text: string, senderIndex: number}) => {
+    return <>
+        
+        <p className='message'>
+            <strong className='message_text'>{text} </strong> 
+            <p className='sender'> {currentSender} </p>
+        </p>
+        </>;
+    }
+*/
 export default Message;
